15c0a9972e3dd2851c784051236bb5cdde889d52
zouelf <elfahemzouhair@gmail.com>
2024-04-13 10:01:33 -0700

    update

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,2 @@
+-- Creates the database hbtn_0c_o.
 create database if not exists 'hbtn_0c_0';
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- Creates a table called first_table values id and name.
+CREATE TABLE IF NIT EXISTS 'first_table' ('id' INT,'name' VARCHAR(256));
