85cebc47446323e5e11f64ed09fcfd707ccad405
zouelf <elfahemzouhair@gmail.com>
2024-04-23 09:33:06 -0700

    update

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,6 +1,3 @@
 -- Lists all records in the table second_table with a score >= 10.
 -- Records are ordered by descending score.
-SELECT 'score', 'name'
-FROM 'second_table'
-WHERE 'score' >= 10.
-ORDER BY 'scor' DESK;
+SELECT score, name FROM second_table  WHERE score >= 10 ORDER BY score DESK;
